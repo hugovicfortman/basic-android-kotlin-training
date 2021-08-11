c9fc17d86678f18c08336c2adc53bfefe8642175
Victor Fortman <Hugovicfortman@outlook.com>
2021-08-11 17:35:41 +0200

    Unit Converter Optional app stuck due to unfamiliarity with spinner widget

---
+++ UnitConverter/app/src/main/java/com/example/unitconverter/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.unitconverter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import com.example.unitconverter.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val subjectsSpinner: Spinner = binding.subjectsSpinner
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.subjects_array,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner
+            subjectsSpinner.adapter = adapter
+        }
+    }
+}
+
